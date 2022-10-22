49448a03756b2e2f12084f9981ee1ab11597d556
CapitanFindusFI <am95.dev@gmail.com>
2022-10-22 18:01:09 +0200

    created search and pagination types, updated client with search method

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+import { DiscogsSearchRequest, DiscogsSearchResult } from "./types/search";
 
 class DiscogsClient {
   private _httpClient: AxiosInstance;
@@ -15,7 +16,28 @@ class DiscogsClient {
         ...(axiosConfig.headers || {}),
         authorization: `Discogs token=${apiToken}`,
         "user-agent": userAgent,
-        "accept-encoding": "gzip, deflate",
+        "content-type": "application/json",
+      },
+    });
+  }
+
+  private _get<T>(url: string, config: AxiosRequestConfig = {}): Promise<T> {
+    return this._httpClient.get<T>(url, config).then((r) => r.data);
+  }
+
+  private _post<T>(
+    url: string,
+    data: any,
+    config: AxiosRequestConfig = {}
+  ): Promise<T> {
+    return this._httpClient.post<T>(url, data, config).then((r) => r.data);
+  }
+
+  public search(params: DiscogsSearchRequest): Promise<DiscogsSearchResult> {
+    return this._get<DiscogsSearchResult>("/database/search", {
+      params: {
+        q: params.query,
+        ...params,
       },
     });
   }
